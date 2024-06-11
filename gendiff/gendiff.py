cf4ae238fbae6339652dc9c1529bb72204fba9a4
Sergey Balashov <balashevski@gmail.com>
2024-06-11 23:00:34 +0400

    function generate_diff seems to work OK

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -7,20 +7,26 @@ def generate_diff(file_path1, file_path2):
     except (FileNotFoundError, PermissionError) as err: 
         print(f"Error opening file1: {err}")
         data_1 = None
+    except (json.JSONDecodeError):
+        print(f"Error: The file '{file_path1}' contains invalid JSON.")
+        return None
     try:
         with open(file_path2, 'r') as file2:
             data_2 = json.load(file2)
+    except (json.JSONDecodeError):
+        print(f"Error: The file '{file_path2}' contains invalid JSON.")
+        return None
     except (FileNotFoundError, PermissionError) as err: 
         print(f"Error opening file1: {err}")
         data_2 = None
     if (not isinstance(data_1, dict)) or (not isinstance(data_2, dict)):
-        return ""
+        return "This utitily only works with json files that start and end with curly braces"
     result_str = "{\n"
     ordered_keys_1 = sorted(data_1.keys())
     ordered_keys_2 = sorted(data_2.keys())
     unique_ordered_keys_1_2 = sorted((set(ordered_keys_1)).union(set(ordered_keys_2)))
     for key in unique_ordered_keys_1_2:
-        if key in ordered_keys_1 and key in ordered_keys_1:
+        if key in ordered_keys_1 and key in ordered_keys_2:
             if data_1[key] == data_2[key]:
                 result_str += f"    {key}: {data_1[key]}\n"
             else: 
@@ -30,7 +36,7 @@ def generate_diff(file_path1, file_path2):
             result_str += f"  - {key}: {data_1[key]}\n"
         else:
             result_str += f"  + {key}: {data_2[key]}\n"
-    result_str += "\n}"
+    result_str += "}"
     return result_str
 
 
