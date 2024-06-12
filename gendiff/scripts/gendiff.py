3306fb424bca11052b2b08f1261234e2c4564443
Sergey Balashov <balashevski@gmail.com>
2024-06-12 16:18:15 +0400

    added calling the generate_diff func on cmd line args

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+from gendiff import generate_diff
 
 
 def main():
@@ -9,7 +10,9 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
-    parser.parse_args()
+    args = parser.parse_args()
+    diff_result = generate_diff(args.first_file, args.second_file)
+    print(diff_result)
 
 
 if __name__ == '__main__':
